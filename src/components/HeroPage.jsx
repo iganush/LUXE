514c4378cba23ac8ae3764f29ce4d044202b9fc2
iganush <Anushgandhi33@gmail.com>
2025-08-02 11:42:32 +0530

    set hero image

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -4,12 +4,12 @@ import Heroo from '../assets/images/Heroo.jpg'
 const Heropage = () => {
   return (
     <>
-    <div className="w-full h-180 pt-13 overflow-hidden ">
+    <div className=" w-full h-150 pt-13 overflow-hidden  bg-top bg-no-repeat bg-cover relative">
       <img
         src={Heroo}
         alt="Hero"
         className="w-full h-full object-cover"
-      />
+      />t
     </div>
     </>
   );
